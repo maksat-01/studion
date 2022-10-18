29d6570d14a8a8bd9394550483154c0880498964
aknazarDev <aknazardev@gmail.com>
2022-10-18 18:39:20 +0600

    init

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,6 +6,7 @@ const Footer = () => {
     return (
         <footer id="footer">
             <div className="footer">
+                <h1>rolan</h1>
                 <div className="footer--general">
                     <div className="footer--general__block">
                         <h1>Form <br/>us <br/>about <br/>your project</h1>
